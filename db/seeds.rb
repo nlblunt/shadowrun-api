e19c209bb450474672f8128d2e6395d974a8ceb3
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2019-02-21 15:57:18 -0800

    Metatype dB seed
    Added seed values for Metatypes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Metatype.create(name: 'Human', body_start: 1, body_max: 1, agility_start: 1, agility_max: 1,
-reaction_start: 1, reaction_max: 1, strength_start: 1, strength_max: 1, willpower_start: 1, willpower_max: 1,
-logic_start: 1, logic_max: 1, intuition_start: 1, intuition_max: 1, charisma_start: 1, charisma_max: 1,
+Metatype.create(name: 'Human', body_start: 1, body_max: 6, agility_start: 1, agility_max: 6,
+reaction_start: 1, reaction_max: 6, strength_start: 1, strength_max: 6, willpower_start: 1, willpower_max: 6,
+logic_start: 1, logic_max: 6, intuition_start: 1, intuition_max: 6, charisma_start: 1, charisma_max: 6,
+edge_start: 2, edge_max: 7)
+
+Metatype.create(name: 'Orc', body_start: 4, body_max: 9, agility_start: 1, agility_max: 6,
+reaction_start: 1, reaction_max: 6, strength_start: 3, strength_max: 8, willpower_start: 1, willpower_max: 6,
+logic_start: 1, logic_max: 5, intuition_start: 1, intuition_max: 6, charisma_start: 1, charisma_max: 5,
 edge_start: 1, edge_max: 1)
 
-Metatype.create(name: 'Orc', body_start: 1, body_max: 1, agility_start: 1, agility_max: 1,
-reaction_start: 1, reaction_max: 1, strength_start: 1, strength_max: 1, willpower_start: 1, willpower_max: 1,
-logic_start: 1, logic_max: 1, intuition_start: 1, intuition_max: 1, charisma_start: 1, charisma_max: 1,
-edge_start: 1, edge_max: 1)
+Metatype.create(name: 'Elf', body_start: 1, body_max: 6, agility_start: 2, agility_max: 7,
+reaction_start: 1, reaction_max: 6, strength_start: 1, strength_max: 6, willpower_start: 1, willpower_max: 6,
+logic_start: 1, logic_max: 6, intuition_start: 1, intuition_max: 6, charisma_start: 3, charisma_max: 8,
+edge_start: 1, edge_max: 6)
 
-Metatype.create(name: 'Elf', body_start: 1, body_max: 1, agility_start: 1, agility_max: 1,
-reaction_start: 1, reaction_max: 1, strength_start: 1, strength_max: 1, willpower_start: 1, willpower_max: 1,
-logic_start: 1, logic_max: 1, intuition_start: 1, intuition_max: 1, charisma_start: 1, charisma_max: 1,
-edge_start: 1, edge_max: 1)
+Metatype.create(name: 'Dwarf', body_start: 3, body_max: 8, agility_start: 1, agility_max: 6,
+reaction_start: 1, reaction_max: 5, strength_start: 3, strength_max: 8, willpower_start: 2, willpower_max: 7,
+logic_start: 1, logic_max: 6, intuition_start: 1, intuition_max: 6, charisma_start: 1, charisma_max: 6,
+edge_start: 1, edge_max: 6)
 
-Metatype.create(name: 'Dwarf', body_start: 1, body_max: 1, agility_start: 1, agility_max: 1,
-reaction_start: 1, reaction_max: 1, strength_start: 1, strength_max: 1, willpower_start: 1, willpower_max: 1,
-logic_start: 1, logic_max: 1, intuition_start: 1, intuition_max: 1, charisma_start: 1, charisma_max: 1,
-edge_start: 1, edge_max: 1)
+Metatype.create(name: 'Troll', body_start: 5, body_max: 10, agility_start: 1, agility_max: 5,
+reaction_start: 1, reaction_max: 6, strength_start: 5, strength_max: 10, willpower_start: 1, willpower_max: 6,
+logic_start: 1, logic_max: 5, intuition_start: 1, intuition_max: 5, charisma_start: 1, charisma_max: 4,
+edge_start: 1, edge_max: 6)
